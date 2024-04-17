47e6b2e5792f033b17f516887261ea3e6e65005b
Jose Bendezu Cortez <bendezucortezjoseluis@gmail.com>
2024-04-16 23:38:27 -0500

    Cambios en deploy, ultimos retoques

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-import { BrowserRouter,HashRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 
 import './style.css'
 import { HeroesApp } from './HeroesApp'
@@ -10,8 +10,8 @@ import { HeroesApp } from './HeroesApp'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HashRouter>
+    <BrowserRouter>
        <HeroesApp />
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>,
 )
